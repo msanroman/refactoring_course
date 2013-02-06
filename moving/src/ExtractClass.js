c5fdfd056d170b75a937df678046a7a439050ec0
Miguel San Román <endymiion@gmail.com>
2013-02-06 19:55:58 +0100

    Added excersises and extracted DNI class in order to start the moving refactorings.

---
+++ moving/src/ExtractClass.js
@@ -0,0 +1,50 @@
+function DNI(aDNI) {
+
+    this.CORRECT_DNI_LETTER = 'a';
+    this.INVALID_DNI_MESSAGE = 'bad DNI'
+    this.DNI = '';
+
+    this.validateDNI=function(aDNI){
+        var lastCharacterPosition = aDNI.length -1;
+        var lastCharacter = aDNI[lastCharacterPosition];
+        var correctDNI = lastCharacter === this.CORRECT_DNI_LETTER;
+        if (correctDNI)
+            return true
+        else throw this.INVALID_DNI_MESSAGE;
+    };
+
+
+    this.toString = function() {
+        return this.DNI;
+    };
+
+    this.validateDNI(aDNI);
+    this.DNI=aDNI;
+}
+
+
+function Persona() {
+    this.name='';
+    this.weight='';
+    this.height='';
+    this.work='';
+    this.bloodType=''
+    this.DNI='';
+
+    this.setDNI = function(aDNI) {
+        this.DNI = new DNI(aDNI);
+    };
+
+    this.getDNI = function() {
+        return this.DNI.toString();
+    };
+
+    this.gainWeight=function(someKilos){};
+
+    this.looseWeight=function(someKilos){};
+
+    this.changeWork=function(newWork){};
+
+    this.newMeasure=function(aHeight){};
+
+}
